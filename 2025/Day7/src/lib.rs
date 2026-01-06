d540ee0648a73e05ab94cb1c54562e66b36ea3d8
NitheshChakaravarthySeelan <albertnitheesh@gmail.com>
2026-01-07 04:20:52 +0530

    feat: implemented part 2 of day 7

--- 2025/Day7/src/lib.rs
+++ 2025/Day7/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::{fs, io, usize};
 
 pub fn read_input(file_path: &str) -> Result<Vec<Vec<char>>, io::Error> {
@@ -46,4 +46,64 @@ pub fn total_splitting(grid: &Vec<Vec<char>>) -> i32 {
         active_cols = next_active_cols;
     }
     split_count
+}
+
+pub fn quantum_splitting(grid: &Vec<Vec<char>>) -> u128 {
+    let m = grid.len();
+    let n = grid[0].len();
+
+    let start_col = grid[0].iter().position(|&c| c == 'S').expect("No S found");
+    let mut memo: HashMap<(usize, usize), u128> = HashMap::new();
+    let result = dfs(0, start_col, grid, m, n, &mut memo);
+
+    result
+}
+
+fn dfs(
+    r: usize,
+    c: usize,
+    grid: &Vec<Vec<char>>,
+    rows: usize,
+    cols: usize,
+    memo: &mut HashMap<(usize, usize), u128>,
+) -> u128 {
+    if let Some(&v) = memo.get(&(r, c)) {
+        return v;
+    }
+
+    // Base case: Reached the end of the grid
+    if r >= rows {
+        return 1; // One timeline successfully completed
+    }
+
+    let tile = grid[r][c];
+    let mut count = 0;
+
+    match tile {
+        'S' | '|' | '.' => {
+            // Particle moves vertically through S, |, or .
+            let nr = r + 1;
+            // The particle always tries to move down.
+            // If nr >= rows, it's handled by the base case above.
+            count = dfs(nr, c, grid, rows, cols, memo);
+        }
+        '^' => {
+            // Splitter: particle goes left and right
+            let nr = r + 1;
+            // Particle attempts to move to (nr, c-1) and (nr, c+1)
+            // Need to check bounds for c-1 and c+1
+            if c > 0 {
+                // Check left path
+                count += dfs(nr, c - 1, grid, rows, cols, memo);
+            }
+            if c + 1 < cols {
+                // Check right path
+                count += dfs(nr, c + 1, grid, rows, cols, memo);
+            }
+        }
+        _ => {}
+    }
+
+    memo.insert((r, c), count);
+    count
 }
--- 2025/Day7/src/main.rs
+++ 2025/Day7/src/main.rs
@@ -1,10 +1,11 @@
 mod lib;
 
-use lib::{read_input, total_splitting};
+use lib::{quantum_splitting, read_input, total_splitting};
 
 fn main() {
     let file_path = "/home/nithesh/WindowsDrive/Coding/Rust/AdventOfCode/2025/Day7/src/input.txt";
     let grid = read_input(file_path).expect("Cant find the file");
-    let total = total_splitting(&grid);
-    print!("{}", total);
+    /// let total = total_splitting(&grid);
+    let tot = quantum_splitting(&grid);
+    print!("{}", tot);
 }
